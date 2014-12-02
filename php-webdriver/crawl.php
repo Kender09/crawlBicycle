26028f7d6cf2ae9d447a67c5ebd03faf247d2adb
tom--bo <otsuka.t.2013@gmail.com>
2014-12-03 01:38:57 +0900

    crawler for fb webcrawler

--- php-webdriver/crawl.php
+++ php-webdriver/crawl.php
@@ -5,14 +5,20 @@ require_once "phpwebdriver/WebDriver.php";
 $webdriver = new WebDriver("localhost", "4444");
 $webdriver->connect("firefox");           
 $webdriver->get("http://www.cb-asahi.jp/shop");
+
+$list = $webdriver->findElementBy(LocatorStrategy::xpath, '//*[@id="contents"]/div[2]/div/div[6]/h3');
+var_dump($list->getValue());
+var_dump($list->getText());
+/*
+$webdriver->get("http://google.com");
+
 $element = $webdriver->findElementBy(LocatorStrategy::name, "q");
 if ($element) {
     $element->sendKeys(array("php webdriver" ) );
     $element->submit();
 }
-/* $list = $webdriver->findElementBy(LocatorStrategy::xpath, "//title/text()"); */
-/* print_r($list); */
-sleep(20);
+ */
+sleep(5);
 
 $webdriver->close();
 
